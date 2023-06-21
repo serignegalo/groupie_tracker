3014beee2cc57cf63b14895e5e22ee3f07575a20
serignegalo <serignegalopro@gmail.com>
2023-06-21 15:03:24 +0000

    Making the .env file for environment variable

--- controllers/utils.go
+++ controllers/utils.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"bufio"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -59,4 +63,24 @@ func Status400(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.Execute(w, struct{ Success bool }{true})
+}
+
+// allows to load the file .env
+func LoadEnv(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println("🚨 " + err.Error())
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			log.Println("🚨 Your env file must be set")
+		}
+		key := parts[0]
+		value := parts[1]
+		os.Setenv(key, value)
+	}
+	return scanner.Err()
 }
--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
 	"groupie-tracker/controllers"
 )
 
 func main() {
-
 	http.HandleFunc("/", controllers.Home)
 	http.HandleFunc("/artist", controllers.GetArtists)
 	http.HandleFunc("/artistDetails", controllers.GetArtistInfo)
 	http.HandleFunc("/relation", controllers.GetRelations)
 	http.HandleFunc("/location", controllers.GetLocations)
 	http.HandleFunc("/date", controllers.GetDates)
+	
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("client/css/"))))
+	
+	controllers.LoadEnv(".env")
+	port := os.Getenv("PORT")
+	http.ListenAndServe(":" + port, nil)
 	fmt.Println("Server starting on port 8080: http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
 }
